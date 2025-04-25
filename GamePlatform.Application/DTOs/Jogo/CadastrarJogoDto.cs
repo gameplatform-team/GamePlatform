d81c787ed0fa09b52e40802cf1e7901c65224f98
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2025-04-25 08:48:40 -0300

    Adiciona atributos de validação a CadastrarJogoDto

--- GamePlatform.Application/DTOs/Jogo/CadastrarJogoDto.cs
+++ GamePlatform.Application/DTOs/Jogo/CadastrarJogoDto.cs
@@ -5,8 +5,10 @@ namespace GamePlatform.Application.DTOs.Jogo;
 public class CadastrarJogoDto
 {
     [Required]
+    [MinLength(1)]
     public string Titulo { get; init; }
     
     [Required]
+    [Range(0.01, 100000.00)]
     public decimal Preco { get; init; }
 }
