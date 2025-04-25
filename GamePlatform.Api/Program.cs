e47d045af0e01bc667d6afaa479f4101179d2109
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2025-04-25 08:23:08 -0300

    Refactor das configurações de autenticação, swagger e logging

--- GamePlatform.Api/Program.cs
+++ GamePlatform.Api/Program.cs
@@ -1,10 +1,12 @@
 using System.Reflection;
 using System.Text;
+using GamePlatform.Api.Configuration;
 using GamePlatform.Api.Middlewares;
 using GamePlatform.Application.Configuration;
 using GamePlatform.Infrastructure.Contexts;
 using GamePlatform.Infrastructure.Seed;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Serilog;
@@ -19,26 +21,10 @@ Log.Logger = new LoggerConfiguration()
 
 builder.Host.UseSerilog();
 
+builder.Services.AddCustomHttpLogging();
+
 // JWT
-var chaveJwt = builder.Configuration["Jwt:Key"];
-
-if (string.IsNullOrEmpty(chaveJwt))
-    throw new InvalidOperationException("A chave JWT (Jwt:Key) n�o est� configurada!");
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveJwt))
-        };
-    });
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 // Adicionar configura��es do banco de dados e servi�os da infraestrutura
 builder.Services.AddInfrastructureServices(builder.Configuration);
@@ -49,40 +35,12 @@ builder.Services.AddApplicationServices();
 // Controllers e Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description =
-            "JWT Authorization Header - utilizado com Bearer Authentication.\r\n\r\n" +
-            "Digite 'Bearer' [espaço] e então seu token no campo abaixo.\r\n\r\n" +
-            "Exemplo (informar sem as aspas): 'Bearer 12345abcdef'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-    });
-    
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            []
-        }
-    });
-    
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+builder.Services.AddSwaggerWithOptions();
 
 var app = builder.Build();
+
+// Middleware de log
+app.UseHttpLogging();
 
 // Chamar o Seed
 using (var scope = app.Services.CreateScope())
